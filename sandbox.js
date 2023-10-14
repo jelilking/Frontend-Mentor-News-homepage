12c6b75f1c0880891e5c2f7358010d8222e861f7
jelilking <abduljelilozigi@gmail.com>
2023-10-14 01:32:38 +0100

    javascript commit

--- sandbox.js
+++ sandbox.js
@@ -23,3 +23,31 @@ for (let i = 0; i < menuItem.length; i++) {
     menu.classList.toggle("hide");
   });
 }
+
+// const toggleMenu = () => {
+//   const iconMenu = document.querySelector(".menu");
+//   const closeMenu = document.querySelector(".close");
+//   const menu = document.querySelector("ul");
+
+//   iconMenu.classList.toggle("hide");
+//   closeMenu.classList.toggle("hide");
+//   menu.classList.toggle("hide");
+// };
+
+// iconMenu.addEventListener("click", toggleMenu);
+// closeMenu.addEventListener("click", toggleMenu);
+
+// for (let i = 0; i < menuItem.length; i++) {
+//   menuItem[i].addEventListener("click", toggleMenu);
+// }
+
+// // Toggle the menu when the user scrolls past a certain point on the page.
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 100) {
+//     toggleMenu();
+//   }
+// });
+
+// // Toggle the menu when the user clicks a button.
+// const toggleMenuButton = document.querySelector(".toggle-menu-button");
+// toggleMenuButton.addEventListener("click", toggleMenu);
